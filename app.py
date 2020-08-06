a634c25600bf1c5f6c2f24747932dd69ca46403b
Daniel Antonio <daniel.antonio@nytimes.com>
2020-08-06 14:59:22 -0400

    returning a dictionary form the endpoint

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ def hello():
 
 @app.route("/api/predict")
 def predict():
+    # get the json from the request
+    # convert the json
+    # pass obj into the function
     return service.predict()
 
 
--- service.py
+++ service.py
@@ -3,16 +3,32 @@ from flask import jsonify, json
 
 def predict():
 
-    # trained model logic goes here...
+    # trained models logic goes here...
+
+    # get all of our predictions
+
+    # arrange into a dictionary
     # 6 different issues
-    prediction = [[0.66, 0.33, 0, 0 , 0]]
-    # retValue = {
-    #
-    # }
+    dictionary = {
+        "Maternal Transfusion": 0.30,
+        "Pernieal Laceration": 0.40,
+        "Ruptured Uterus": 0,
+        "Unplanned Hysterectomy": 0,
+        "Intensive Care": 0.30
+    }
     response = jsonify(
-        response=json.dumps(prediction),
+        response=json.dumps(dictionary),
         status=200,
         mimetype='application/json'
     )
+
+    # handle some errors
+    # if True:
+    #     response = jsonify(
+    #         response=json.dumps("something went wrong"),
+    #         status=500,
+    #         mimetype='application/json'
+    #     )
+
     return response
 
