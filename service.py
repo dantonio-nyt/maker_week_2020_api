8b18eb9b5a223a80869e98107620b73b703dd5fe
Daniel Antonio <daniel.antonio@nytimes.com>
2020-08-06 11:48:25 -0400

    Allow CORS

--- service.py
+++ service.py
@@ -4,11 +4,15 @@ from flask import jsonify, json
 def predict():
 
     # trained model logic goes here...
-
-    prediction = [[0.66, 0.33]]
+    # 6 different issues
+    prediction = [[0.66, 0.33, 0, 0 , 0]]
+    # retValue = {
+    #
+    # }
     response = jsonify(
         response=json.dumps(prediction),
         status=200,
         mimetype='application/json'
     )
     return response
+
